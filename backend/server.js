72a1a3b670d7a4d214a80517d9c44cfd723fa22c
leoAlvTor <calvaradot1@est.ups.edu.ec>
2020-06-30 18:29:47 -0500

    Commit

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,8 @@ mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
-    console.log("MongoDB database connection established successfully");
-})
+  console.log("MongoDB database connection established successfully");
+});
 
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
